924f9fa5236988bafc6dc6eaa72b9be8aeca9b80
Rahul Choudhary <rahul0101rock@gmail.com>
2021-10-09 23:35:49 +0530

    modified routes and loginComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {
-    path: '', redirectTo: 'signup', pathMatch: 'full'
-  }, {
-    path: 'signup',  component: SignupComponent
+    path: '', redirectTo: 'home', pathMatch: 'full'
   }, {
     path: 'login', component: LoginComponent
+  }, {
+    path: 'home', component: HomeComponent
   }
+  
 ];
 
 @NgModule({
