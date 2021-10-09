fd1a7d4e2a21b726e1cd222c1bf4c08523197087
Rahul Choudhary <rahul0101rock@gmail.com>
2021-10-09 19:00:53 +0530

    Added Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,7 +10,7 @@ export class LoginComponent implements OnInit {
   myForm: FormGroup;
   message: string="";
   userError:any;
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder,public auths: AuthService) {
     this.myForm = this.fb.group({
     email: ['',[Validators.required,Validators.email]],
     password: ['',[Validators.required, Validators.minLength(8)]]
@@ -18,6 +18,19 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+  onSubmit(form: any){
+    let email: string = form.value.email;
+    let password: string = form.value.password;
+    this.auths.login(email,password).then((response: any) => {
+      console.log(response);
+      this.message="You have been Logged in Successfully";
+      this.userError=null;
+    }).catch((error: any) =>{
+      console.log(error);
+      this.message="";
+      this.userError=error;
+    });
   }
 
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import * as firebase from 'firebase/app';
-import * as auth from 'firebase/auth';
-import { getAuth, updateProfile } from 'firebase/auth';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -12,7 +10,7 @@ export class SignupComponent implements OnInit {
   myForm: FormGroup;
   message: string="";
   userError:any;
-  constructor(public fb: FormBuilder) {
+  constructor(public fb: FormBuilder,public auths: AuthService) {
     this.myForm = this.fb.group({
       email: ['',[Validators.required,Validators.email]],
       password: ['',[Validators.required, Validators.minLength(8)]],
@@ -29,16 +27,13 @@ export class SignupComponent implements OnInit {
     let password: string = form.value.password;
     let firstName: string = form.value.firstName;
     let lastName: string = form.value.lastName;
-    let fullName: string = firstName + " " + lastName;
-    let au = getAuth();
-    auth.createUserWithEmailAndPassword(au,email,password).then((response) => {
+    this.auths.signup(email,password,firstName,lastName).then((response: any) => {
       console.log(response);
-      updateProfile(response.user, {displayName: fullName,
-         photoURL: "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg"}).then(()=>{
-           this.message="You have been Signed Up Successfully"
-         });
-    }).catch((error) =>{
+      this.message="You have been Signed Up Successfully";
+      this.userError=null;
+    }).catch((error: any) =>{
       console.log(error);
+      this.message="";
       this.userError=error;
     });
   }
