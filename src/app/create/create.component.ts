6d24d1d7ea59521453b4fddde1d6b21331579325
Rahul Choudhary <rahul0101rock@gmail.com>
2021-10-11 01:29:50 +0530

    Added createpost methord for uploading data in fireStore

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import * as db from'firebase/firestore';
+import * as auth from 'firebase/auth';
 import { Editor } from 'ngx-editor';
+import { addDoc, serverTimestamp } from 'firebase/firestore';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  title = ''; 
+  title = '';
+  content = '';
   editor: Editor = new Editor();
   html = '';
   constructor() { }
@@ -18,6 +20,18 @@ export class CreateComponent implements OnInit {
     this.editor = new Editor();
   }
   createPost() {
-    
-  }
+    console.log(this.title,this.content);
+    const fr = db.getFirestore();
+    addDoc(db.collection(fr,"posts"),{
+      title: this.title,
+      content: this.content,
+      owner: auth.getAuth().currentUser?.uid,
+      created: serverTimestamp()
+    }).then((data: any) => {
+      console.log(data);
+    })
+    .catch((error: any) => {
+      console.log(error);
+    });
+}
 }
