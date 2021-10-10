2b73b5d34536d8618ccf9b9f89423708ba639014
Rahul Choudhary <rahul0101rock@gmail.com>
2021-10-11 05:03:25 +0530

    Added user collection for edit profile component and validator for pass matching

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import * as firebase from 'firebase/app';
+import * as auth from 'firebase/auth';
+import * as db from 'firebase/firestore'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -10,13 +14,17 @@ export class SignupComponent implements OnInit {
   myForm: FormGroup;
   message: string="";
   userError:any;
-  constructor(public fb: FormBuilder,public auths: AuthService) {
+  constructor(public fb: FormBuilder,public auths: AuthService,public router: Router) {
     this.myForm = this.fb.group({
       email: ['',[Validators.required,Validators.email]],
       password: ['',[Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]],
       firstName: ['',[Validators.required]],
       lastName: ['',[Validators.required]]
-    });
+    },{
+      validator: this.matchpass("password", "confirmPassword")
+    }
+    );
    }
 
   ngOnInit(): void {
@@ -27,14 +35,41 @@ export class SignupComponent implements OnInit {
     let password: string = form.value.password;
     let firstName: string = form.value.firstName;
     let lastName: string = form.value.lastName;
-    this.auths.signup(email,password,firstName,lastName).then((response: any) => {
-      console.log(response);
-      this.message="You have been Signed Up Successfully";
-      this.userError=null;
+    this.auths.signup(email,password,firstName,lastName).then((user: any) => {
+
+      db.setDoc(db.doc(db.collection(db.getFirestore(),"users"),user.uid),
+      {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        photoURL: user.photoURL,
+        interests: "",
+        bio: "",
+        hobbies: ""
+      }).then(() => {
+        this.message = "You have been signed up successfully.";
+        this.userError = null;
+        this.router.navigate(['/myblogs'])
+      })
     }).catch((error: any) =>{
       console.log(error);
       this.message="";
       this.userError=error;
     });
+  }
+
+  matchpass(passwordKey: string, confirmPasswordKey: string){
+    return (group: FormGroup) => {
+      let password = group.controls[passwordKey];
+      let confirmPassword = group.controls[confirmPasswordKey];
+      if(password.value == confirmPassword.value){
+        return;
+      } else {
+        confirmPassword.setErrors({
+          notEqualToPassword: true
+        })
+      }
+
+    }
   }
 }
